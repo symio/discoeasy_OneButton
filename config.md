9165409bef801e94799bb3b755fe7d55725084fa
Huby Franck <franck.huby@loamok.org>
2018-12-15 17:50:42 +0100

    Mise à jour suite à sortie du pack bicolore et mise à jour par dagoma Ajout de config possible et correction d'un bug probable

---
+++ config.md
@@ -0,0 +1,59 @@
+# Loamok ModedEasy200 configuration
+
+Allez hop nouvelle version vous pouvez configurer : 
+
+ - Le nom de votre imprimante 
+ - Le nivelage à 9 points de palpage (non proposé par Dagoma) 
+ - Le bouton pause sans dépendance de l'extrudeur+ 
+ - L'inversion des ports des extrudeurs
+ - Les trapézoïdales sans pack expert 
+ - Les pid x,y,z et E 
+ 
+ - L’inversion des ports des extrudeurs (non proposé par Dagoma)
+## Le nom de l'imprimante :
+Dé-commentez la ligne 3 et changez (si vous le voulez) le nom ici je vous propose "ModedEasy200" :
+
+    #define MODED_MACHINE_NAME ModedEasy200
+
+## leveling en 9 points
+Dé-commentez la ligne 4 pour activer le leveling en 9 points (plus précis que les 5 points de Dagoma) :
+
+    #define MOD_BED_LEVELING_9P
+
+## Le bouton pause sans dépendance de l'extrudeur+
+Dé-commentez la ligne 6 pour activer le bouton pause non dépendant du détecteur de filament :
+
+    #define ONE_BUTTON_MOD
+
+## Inversion des ports extrudeurs
+Vous pouvez inverser les ports d'extrudeurs (E1 en E0 et vice et versa) en dé-commentant la ligne 7
+
+    #define MODED_INVERT_EXTRUD
+
+## Inversion des ports extrudeurs
+Vous pouvez inverser les ports d'extrudeurs (E1 en E0 et vice et versa) en dé-commentant la ligne 7
+
+    #define MODED_INVERT_EXTRUD
+
+## Les trapézoïdales sans pack expert 
+Vous pouvez définir votre imprimante comme utilisant des tiges trapézoïdales sans pack expert en dé-commentant la ligne 8 (configurez aussi les pid en dessous à 80, 80, 400, 98 pour les trapez)
+
+    #define MODED_TRAPEZ
+
+## Les pid x,y,z et E 
+Vous pouvez définir les pid de votre imprimante en dé-commentant la ligne 9 et en les configurant lignes 13 à 17 
+
+    #define MODED_PRECISION
+    #define MODED_PID_X 80 // X Par défaut la valeur dagoma est 80
+    #define MODED_PID_Y 80 // Y Par défaut la valeur dagoma est 80
+    #define MODED_PID_Z 2560// Z Par défaut la valeur dagoma est 400 
+    #define MODED_PID_E 98  // E pour extrudeur
+Les valeurs sont :
+
+ - X Dagoma : 80 ; X chez moi : 79.4934
+ - Y Dagoma : 80 ; Y chez moi : 79.2864
+ - Z Dagoma filetés : 2560 ; Z Dagoma Trapez (pack expert) : 400  ; Y chez moi : 397.2195
+ - E Dagoma : 98 ; E chez moi : 100.8023
+Reportez vous à cette discussion pour bien régler vos pid :
+https://www.lesimprimantes3d.fr/forum/topic/3394-tuto-calcul-des-steps-per-mmunit-une-fois-pour-toutes/
+
--- moded_easy.h
+++ moded_easy.h
@@ -1,11 +1,30 @@
 #ifndef MODED_EASY_H
   #define MODED_EASY_H
-  //#define MOD_BED_LEVELING_9P // (décommentez (enlevez les "//" en début de ligne pour 
-				 // activer le leveling en 9 points))
+    #define MODED_MACHINE_NAME ModedEasy200 // Décommentez pour changer le nom de la machine
+//  #define MOD_BED_LEVELING_9P // décommentez (enlevez les "//" en début de ligne) pour 
+				 // activer le leveling en 9 points
   #define ONE_BUTTON_MOD	 // active ou désactive le mod one button
+//  #define MODED_INVERT_EXTRUD // invertion extrudeur 0 en extrudeur 1
+//  #define MODED_TRAPEZ        // Z en trapézoïdales sans pack expert
+//  #define MODED_PRECISION     // Définition de la précision des prints 
+                                // (très utile pour personaliser les pid)
+//  Et définissez vos pid ci-dessous voir :
+//  https://www.lesimprimantes3d.fr/forum/topic/3394-tuto-calcul-des-steps-per-mmunit-une-fois-pour-toutes/
+    #define MODED_PID_X 80      // X Par défaut la valeur dagoma est 80
+    #define MODED_PID_Y 80      // Y Par défaut la valeur dagoma est 80
+    #define MODED_PID_Z 2560    // Z Par défaut la valeur dagoma est 400 
+                                // en trapez pack expert et 2560 en filetés standard 
+    #define MODED_PID_E 98      // E (extru) Par défaut la valeur dagoma est 98
 #endif
+/*-----------------------------------------------------------------------------*/
+// Fin de config ne touchez à rien ci-dessous configurez ce que vous voulez au dessus de cette ligne
+/*-----------------------------------------------------------------------------*/
 #ifdef MODED_EASY_BOARD
   // Configuration.h
+  #ifdef MODED_MACHINE_NAME
+    #undef CUSTOM_MACHINE_NAME
+    #define CUSTOM_MACHINE_NAME MODED_MACHINE_NAME
+  #endif
   #ifdef MOD_BED_LEVELING_9P
   // palpage en 9 points avec meilleure prise en compte de la taille du plateau
     #undef LEFT_PROBE_BED_POSITION
@@ -20,7 +39,7 @@
     #define AUTO_BED_LEVELING_GRID_POINTS 3
   #endif
 
-  #if defined(ONE_BUTTON_MOD) && !ENABLED(FILAMENT_RUNOUT_SENSOR)
+  #if defined(ONE_BUTTON_MOD) && !ENABLED(FILAMENT_RUNOUT_SENSOR) && !defined(ONE_BUTTON)
   // one button mod
     #define ONE_BUTTON_INVERTING true
     #define ONE_BUTTON
@@ -33,5 +52,36 @@
     #define SUMMON_PRINT_PAUSE_PIN 18
     #undef ONE_BUTTON_PIN
     #define ONE_BUTTON_PIN SUMMON_PRINT_PAUSE_PIN
+  #endif
+  #ifdef MODED_INVERT_EXTRUD
+  // invertion du port extrudeur (si vous n'avez pas modifié le branchement de votre 
+  // extrudeur commentez la ligne 4)
+  // pins_RAMPS_14.h
+    #undef E1_STEP_PIN
+    #define E1_STEP_PIN        26
+    #undef E1_DIR_PIN
+    #define E1_DIR_PIN         28
+    #undef E1_ENABLE_PIN
+    #define E1_ENABLE_PIN      24
+
+    #undef E0_STEP_PIN
+    #define E0_STEP_PIN        36
+    #undef E0_DIR_PIN
+    #define E0_DIR_PIN         34
+    #undef E0_ENABLE_PIN
+    #define E0_ENABLE_PIN      30
+  #endif
+  #ifdef MODED_TRAPEZ
+    #undef DEFAULT_AXIS_STEPS_PER_UNIT
+    #ifdef MODED_PRECISION
+      #define DEFAULT_AXIS_STEPS_PER_UNIT {MODED_PID_X,MODED_PID_Y,MODED_PID_Z,MODED_PID_E}
+    #else
+      #define DEFAULT_AXIS_STEPS_PER_UNIT {MODED_PID_X,MODED_PID_Y,MODED_PID_Z,MODED_PID_E}
+    #endif
+  #else
+    #ifdef MODED_PRECISION
+      #undef DEFAULT_AXIS_STEPS_PER_UNIT
+      #define DEFAULT_AXIS_STEPS_PER_UNIT {MODED_PID_X,MODED_PID_Y,MODED_PID_Z,MODED_PID_E}
+    #endif
   #endif
 #endif
